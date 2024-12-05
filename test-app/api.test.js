cc74bbf7c7fb0dc6fa31ff08763b0b54a74ee474
nassro2001 <104642625+nassro2001@users.noreply.github.com>
2024-12-05 15:09:07 +0200

    Update api.test.js 2

--- test-app/api.test.js
+++ test-app/api.test.js
@@ -30,7 +30,7 @@ describe('API Tests', function () {
 
   it('should create a new tutorial', async function () {
     const data = {
-      title: 'second tutorial',
+      title: 'third tutorial',
       description: 'this is the description of the first tutorial',
       published: false
     };
